c2ee3880b2ffbbde8a935257583717c88ed31b5f
Namide <damien@doussaud.fr>
2024-09-04 08:44:33 +0200

    Add not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import HomeView from "../views/HomeView.vue";
 import DemoView from "../views/DemoView.vue";
 import GuideView from "../views/GuideView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 
 export const routes = [
   {
@@ -17,5 +18,10 @@ export const routes = [
     path: "/guide",
     name: "guide",
     component: GuideView,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "not-found",
+    component: NotFoundView,
   },
 ] as const;
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useHead } from "@unhead/vue";
+
+useHead({
+  title: "Extract colors | Page non trouvée",
+  meta: [
+    {
+      name: "description",
+      content: "Javascript library to extract color palette from images",
+    },
+  ],
+});
+</script>
+
+<template>
+  <div class="flex flex-1 justify-center items-center">
+    <div class="text-base-content pl-2 pr-10 pt-2 pb-10">
+      <h1 class="text-xl font-bold">Page non trouvée</h1>
+      <p class="opacity-80">Cette page n'existe pas.</p>
+    </div>
+  </div>
+</template>
